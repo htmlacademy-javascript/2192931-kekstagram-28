c12879745e90088c061e95b41877814154198167
Aleksey <111198139+MrTruth34@users.noreply.github.com>
2023-03-01 23:59:42 +0300

    Исправление ошибок

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,11 @@
 const checkString = function (firstNumber, secondNumber) {
   return firstNumber.length <= secondNumber;
 };
+
+checkString('проверяемая строка', 20);
+
 // Функция для проверки, является ли строка палиндромом
-const seachWord = function (wordFirst) {
+const searchWord = function (wordFirst) {
   const tempString = wordFirst.toLowerCase()
     .replaceAll(' ', '');
   let reverseString = '';
@@ -12,6 +15,8 @@ const seachWord = function (wordFirst) {
   }
   return tempString === reverseString;
 };
+
+searchWord('топот');
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 const extractNumber = function (string) {
@@ -28,11 +33,15 @@ const extractNumber = function (string) {
   return parseInt(result, 10);
 };
 
+extractNumber('2023 год');
+
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку
-const myPadStart = function(string, minLength, pad){
-  const actualPad = minLength - string.length ;
-  if(actualPad <= 0){
+const myPadStart = function (string, minLength, pad) {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
     return string;
   }
   return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
 };
+
+myPadStart('1', 2, '0');
